bc54eb703bd76aee6e74c2dd34f4da70f5c88d38
Bryan Barreto <bryan.soares@santander.com.br>
2021-03-26 13:41:10 -0300

    creating httpMethods enum

---
+++ Sources/EasyRequest/HttpMethods.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bryan Barreto Soares on 26/03/21.
+//
+
+import Foundation
+
+public enum HttpMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    
+    public var value: String {
+        return self.rawValue
+    }
+}
