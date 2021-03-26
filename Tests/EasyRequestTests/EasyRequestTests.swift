722f20a01f307f88dade766e4133f02313fb3f3d
Bryan Barreto <bryan.soares@santander.com.br>
2021-03-26 13:58:31 -0300

    removing standard unit test

--- Tests/EasyRequestTests/EasyRequestTests.swift
+++ Tests/EasyRequestTests/EasyRequestTests.swift
@@ -3,10 +3,7 @@ import XCTest
 
 final class EasyRequestTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(EasyRequest().text, "Hello, World!")
+        
     }
 
     static var allTests = [
